d19b4bd7b49c4e9ac8e584d7b7bcc412294b40fd
Divyarajalakshmi Natarajan <divyarajalakshmin@gmail.com>
2016-05-17 13:49:06 -0700

    finding space of db

--- help-sql.sql
+++ help-sql.sql
@@ -1,1 +1,5 @@
 -- helped sql queries.
+
+-- find the size of the db.
+select (select sum(bytes)/1024/1024 from dba_data_files)+
+(select sum(bytes)/1024/1024 from dba_temp_files) "Size in MB" from dual;
